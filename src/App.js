71ecad278e62f6c58dd81c70c68ba3f7f4612b2e
ZeynepST <zstasoglu@gmail.com>
2025-06-25 13:51:15 -0400

    add: navigation bar for easier navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import {BrowserRouter as Router, Route,Routes} from 'react-router-dom';
-import {ModelProvider} from './view-model/ModelContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ModelProvider } from './view-model/ModelContext';
 import AgentSwitchBoardPage from './view/AgentSwitchBoard';
 import DebuggingPage from './view/DebuggingPage';
 import ReportDataPage from './view/ReportDataPage';
 import '@blueskyproject/finch/style.css';
 import './stylesheets/index.css';
 import FinchPage from './view/FinchPage';
+import NavBar from './components/NavigationBar';
 
-function App(){
+function App() {
 
-    return(
-            <ModelProvider>
+    return (
+        <ModelProvider>
+            <div className="main-page">
+                <NavBar />
                 <Routes>
                     <Route path="/" element={<AgentSwitchBoardPage />} />
-                    <Route path="/DebuggingPage" element={<DebuggingPage />}/>
-                    <Route path="/ReportDataPage" element={<ReportDataPage />}/>
-                    <Route path="/FinchPage" element={<FinchPage />}/>
-
+                    <Route path="/DebuggingPage" element={<DebuggingPage />} />
+                    <Route path="/ReportDataPage" element={<ReportDataPage />} />
+                    <Route path="/FinchPage" element={<FinchPage />} />
                 </Routes>
-                
-            </ModelProvider>
-     );
+            </div>
+        </ModelProvider>
+    );
 };
 
 export default App;
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import '../stylesheets/NavBar.css';
+
+const NavBar = () => {
+
+    return (
+        <nav className="navbar">
+            <h1 className="logo"> </h1>
+            <ul className="nav-links">
+                <li><Link to="/">Switchboard</Link></li>
+                <li><Link to="/DebuggingPage">Methods/Variables</Link></li>
+                <li><Link to="/ReportDataPage">UID</Link></li>
+                <li><Link to="/FinchPage">Finch</Link></li>
+            </ul>
+        </nav>
+
+    );
+
+}
+
+export default NavBar;
