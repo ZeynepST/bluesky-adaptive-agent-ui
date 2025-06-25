37e50468d41055d78868307458355053ac09308e
ZeynepST <zstasoglu@gmail.com>
2025-06-25 10:41:32 -0400

    Add: add buttons for page navigation to and from ReportDataPage.js

--- src/view/ReportDataPage.js
+++ src/view/ReportDataPage.js
@@ -1,19 +1,18 @@
 import React, { useState, useContext, useCallback, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { ModelContext } from "../view-model/ModelContext";
-import { Tiled } from '@blueskyproject/finch';
-
-
+// import { Tiled } from '@blueskyproject/finch';
 
 const ReportDataPage = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <Tiled tiledBaseUrl='/api/v1'></Tiled>
+        <div className="report-data-page-container">
             <div className="switch-btn-container">
-                <button className="switchboard-btn" onClick={() => navigate("/DebuggingPage")}>Switchboard</button>
+                <button className="switchboard-btn" onClick={() => navigate("/")}>Switchboard</button>
+                <button className="switchboard-btn" onClick={() => navigate("/DebuggingPage")}>Methods/Variables</button>
             </div>
+
         </div>
     );
 
