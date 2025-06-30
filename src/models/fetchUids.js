ce3589e8270cb83f62ed5d21eb9c06c2d550d0c5
ZeynepST <zstasoglu@gmail.com>
2025-06-30 13:05:10 -0400

    Refactor: query list of UIDs by "MockClusterAgent" and remove unnecessary props passed to IngestDataPage and ReportDataPage

--- src/models/fetchUids.js
+++ src/models/fetchUids.js
@@ -2,33 +2,37 @@
 import axios from "axios";
 
 export const get_uids = async () => {
-        try {
-            const response = await axios.get('/api/v1/search/');
-            // This is to access the nested data within the response
-            const rawData = response.data.data;
-            // This is parsing the nested data 
-            return rawData.map((item) => {
-                const uidValue = item.id;
-                const metadata = item.attributes.metadata;
-                const model_params = item.attributes.metadata.start.model_params;
-                // streamNames refers to ingest and report.
-                const streamNames = metadata?.summary?.stream_names || [];
 
-                return {
-                    uidValue,
-                    datetime: metadata?.summary?.datetime || null,
-                    hasIngest: streamNames.includes("ingest"),
-                    hasReport: streamNames.includes("report"),
-                    agentName: metadata?.start?.agent_name || null,
-                    modelType: metadata?.start?.model_type || null,
-                    modelAlgorithm: model_params?.algorithm || null,
-                    maxIter: model_params?.max_iter || null,
-                    numberOfClusters: model_params?.n_clusters || null,
-                    randomState: model_params?.random_state || null,
-                };
-            });
-        }
-        catch (error) {
-            console.error("Failed to get list of uids");
-        }
+    try {
+        const response = await axios.get('/api/v1/search/');
+        // This is to access the nested data within the response
+        const rawData = response.data.data;
+        // This is parsing the nested data and ensures only MockClusterAgents are processed 
+        const parsedUids = rawData.filter(item => item?.attributes?.metadata?.start?.agent_name?.startsWith("MockClusterAgent")).map((item) => {
+            const uidValue = item.id;
+            const metadata = item.attributes.metadata;
+            const model_params = item.attributes.metadata.start.model_params;
+            // streamNames refers to ingest and report.
+            const streamNames = metadata?.summary?.stream_names || [];
+
+            return {
+                uidValue,
+                datetime: metadata?.summary?.datetime || null,
+                hasIngest: streamNames.includes("ingest"),
+                hasReport: streamNames.includes("report"),
+                agentName: metadata?.start?.agent_name || null,
+                modelType: metadata?.start?.model_type || null,
+                modelAlgorithm: model_params?.algorithm || null,
+                maxIter: model_params?.max_iter || null,
+                numberOfClusters: model_params?.n_clusters || null,
+                randomState: model_params?.random_state || null,
+            };
+
+        });
+        return parsedUids;
     }
+    catch (error) {
+        console.error("Failed to get list of uids");
+        return [];
+    }
+}
--- src/view/UidPage.js
+++ src/view/UidPage.js
@@ -16,40 +16,44 @@ const UidPage = () => {
     const navigate = useNavigate();
     const { uidsInfo } = useContext(UidContext);
     const { viewMode, uidValue } = useParams();
+    const chosenUidObject = uidValue
+        ? uidsInfo?.find(uid => uid.uidValue === uidValue)
+        : null;
 
-
-    const chosenUidObject = uidsInfo.find(uid => uid.uidValue === uidValue);
-   // const uidValue = chosenUidObject?.uidValue;
+    // /const chosenUidObject = uidsInfo.find(uid => uid.uidValue === uidValue);
 
     return (
-        <div className="report-data-page-container">
-            <div className="report-data-main-layout">
-                <SideBar />
-                <div className="report-data-main-content">
-                    {chosenUidObject !== null && (
-                        <>
-                            <UidBanner uidObject={chosenUidObject} viewMode={viewMode} />
-                            <div className="report-data-main-content-container">
-                                {/* If a UID object is chosen, the render will depend on whether Report or Ingest was selected */}
-                                {
-                                    viewMode === "ingest" && (
-                                        <IngestDataPage uid={uidValue} />
-                                    )
-                                }
-                                {
-                                    viewMode === "report" && (
-                                        <ReportDataPage uid={uidValue} />
-                                    )
-                                }
-
-                            </div>
-                        </>
-                    )}
+        <>
+            {uidsInfo && (
+                <div className="report-data-page-container">
+                    <div className="report-data-main-layout">
+                        <SideBar />
+                        <div className="report-data-main-content">
+                            {chosenUidObject !== null && (
+                                <>
+                                    <UidBanner uidObject={chosenUidObject} viewMode={viewMode} />
+                                    <div className="report-data-main-content-container">
+                                        {/* If a UID object is chosen, the render will depend on whether Report or Ingest was selected */}
+                                        {
+                                            viewMode === "ingest" && (
+                                                // <IngestDataPage uid={uidValue} />
+                                                <IngestDataPage />
+                                            )
+                                        }
+                                        {
+                                            viewMode === "report" && (
+                                                <ReportDataPage />
+                                            )
+                                        }
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            )}
+        </>
     );
-
 }
 
 export default UidPage;
