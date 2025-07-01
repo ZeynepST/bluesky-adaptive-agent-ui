fd44ef83a6ad37776a3ace702a2e63011a989960
ZeynepST <zstasoglu@gmail.com>
2025-07-01 14:09:00 -0400

    Add: add PlotlyScatter.tsx from Finch repo and make it compatible with react application.

---
+++ src/components/PlotlyScatter.js
@@ -0,0 +1,72 @@
+import React, { useRef, useEffect, useState } from 'react';
+import Plot from 'react-plotly.js';
+
+// will need to add licensing 
+export default function PlotlyScatter({
+  data,
+  title,
+  xAxisTitle,
+  yAxisTitle,
+  xAxisRange,
+  yAxisRange,
+  xAxisLayout,
+  yAxisLayout,
+  className,
+}) {
+  const plotContainer = useRef(null);
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      if (entries[0]) {
+        const { width, height } = entries[0].contentRect;
+        setDimensions({ width, height });
+      }
+    });
+
+    if (plotContainer.current) {
+      resizeObserver.observe(plotContainer.current);
+    }
+
+    return () => resizeObserver.disconnect();
+  }, []);
+
+
+
+  return (
+    <div className={`pb-4 ${className ?? ''}`} ref={plotContainer}>
+      <Plot
+        data={data}
+        layout={{
+          title: {
+            text: title
+          },
+          xaxis: {
+            title: {
+              text: xAxisTitle
+            },
+            range: xAxisRange,
+            ...xAxisLayout,
+          },
+          yaxis: {
+            title: {
+              text: yAxisTitle
+            },
+            range: yAxisRange,
+            ...yAxisLayout,
+          },
+          autosize: true,
+          width: dimensions.width,
+          height: dimensions.height,
+          margin: {
+            l: yAxisTitle ? 60 : 30,
+            r: 30,
+            t: title ? 40 : 30,
+            b: xAxisTitle ? 60 : 30,
+          },
+        }}
+        config={{ responsive: true }}
+      />
+    </div>
+  );
+}
