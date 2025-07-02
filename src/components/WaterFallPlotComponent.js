262cc727cdce7027fdc7472c3bc51cc3b84c0132
ZeynepST <zstasoglu@gmail.com>
2025-07-02 12:05:38 -0400

    Add: added an input where user can adjus the offset for the waterfall plot and made the plot have a set height to make it stable

--- src/components/WaterFallPlotComponent.js
+++ src/components/WaterFallPlotComponent.js
@@ -1,9 +1,15 @@
 import Plot from 'react-plotly.js';
+import { useState } from 'react';
+import '../stylesheets/PlotStylesheets/WaterFallPlotComponent.css';
 
 function WaterFallPlot({ observables }) {
+
+  //the offset for the waterfall plot will be determined by the user
+  const [offSet, setOffSet] = useState("");
+
   // offsetObservables is an array where the value of each row is shifted upward by its index
   const offsetObservables = observables.map((row, rowIndex) =>
-    row.map(value => value + rowIndex)
+    row.map(value => value + (rowIndex *( Number(offSet) || 1)))
   );
 
   //singeLine represents each line on the plot
@@ -16,43 +22,59 @@ function WaterFallPlot({ observables }) {
   }));
 
   return (
-    <Plot
-      data={singleLine}
-      layout={{
-        title: {
-          text: 'Waterfall Plot of Observables',
-          x: 0.5,
-          xanchor: 'center',
-          font: {
-            size: 15
-          }
-        },
-        xaxis: {
-          title: {
-            text: 'Index',
-            font: {
-              size: 10
-            }
-          }
-        },
-        yaxis: {
-          title: {
-            text: 'Value',
-            font: {
-              size: 10
-            }
-          }
-        },
-        // height: 400,
-        // showlegend: false,
-        autosize: true,
-        margin: { l: 40, r: 20, t: 40, b: 40 },
-      }}
+    <div className="waterfall-plot-container">
+      <div className="waterfall-plot-wrapper">
+        {/* beginning of plot */}
+        <Plot
+          data={singleLine}
+          layout={{
+            title: {
+              text: 'Waterfall Plot of Observables',
+              x: 0.5,
+              xanchor: 'center',
+              font: {
+                size: 15
+              }
+            },
+            xaxis: {
+              title: {
+                text: 'Index',
+                font: {
+                  size: 10
+                }
+              }
+            },
+            yaxis: {
+              title: {
+                text: 'Value',
+                font: {
+                  size: 10
+                }
+              }
+            },
+            autosize: true,
+            margin: { l: 40, r: 20, t: 40, b: 40 },
+          }}
 
-      config={{ responsive: true }}
-      useResizeHandler={true}
-      style={{ width: '100%', height: '100%' }}
-    />
+          config={{ responsive: true }}
+          useResizeHandler={true}
+          // keeping height at a set value makes it stable and no longer overlaps the input area 
+          style={{ width: '100%', height: 500}}
+        />
+        {/* end of plot */}
+
+
+        <div className="waterfall-plot-offset-input-box" >
+          <input
+            type="number"
+            className="offset-input"
+            placeholder="Enter Offset..."
+            value={offSet}
+            onChange={(e) => setOffSet(Number(e.target.value))}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
